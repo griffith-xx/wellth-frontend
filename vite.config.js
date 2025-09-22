910b85e7d0cab30034e547e4032e9e40a5dc93ec
灶门炭治郎的歌曲 <chacritpoonpipit@gmail.com>
2025-09-22 22:22:52 +0700

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,39 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
-import Components from 'unplugin-vue-components/vite';
-import { PrimeVueResolver } from '@primevue/auto-import-resolver';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig, loadEnv } from "vite";
+import Components from "unplugin-vue-components/vite";
+import { PrimeVueResolver } from "@primevue/auto-import-resolver";
+import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), "");
+
+    return {
+        plugins: [
+            laravel({
+                input: "resources/js/app.js",
+                ssr: "resources/js/ssr.js",
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+            Components({
+                resolvers: [PrimeVueResolver()],
+            }),
+            tailwindcss(),
+        ],
+        server: {
+            host: env.PRIVATE_IP || "localhost",
+            port: parseInt(env.VITE_PORT) || 5173,
+            hmr: {
+                host: env.PRIVATE_IP || "localhost",
             },
-        }),
-        Components({
-            resolvers: [
-                PrimeVueResolver()
-            ]
-        }),
-        tailwindcss()
-    ],
+        },
+    };
 });
